594b64f172495757bd56ceb9173d87e5bae76f64
vtomi97 <47665690+vtomi97@users.noreply.github.com>
2021-03-04 21:37:59 +0100

    Wrong Branch Fix

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -3,11 +3,7 @@ package hu.bme.mit.train.controller;
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
-
-	/*
-	Bugfix
-	 */
-
+	
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;

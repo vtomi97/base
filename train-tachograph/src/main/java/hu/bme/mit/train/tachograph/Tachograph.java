b8c966c9dc93c6cd2584948982cfecf4fd5a53f3
vtomi97 <47665690+vtomi97@users.noreply.github.com>
2021-03-06 16:15:04 +0100

    Tachograph Created

---
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
@@ -0,0 +1,21 @@
+package hu.bme.mit.train.tachograph;
+
+import com.google.common.collect.Table;
+import com.google.common.collect.TreeBasedTable;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalTime;
+
+public class Tachograph {
+    private Long key = 0L;
+    private Table<Long, Integer, Integer> table = TreeBasedTable.create();
+
+    void add(Integer joystickPosition, Integer referenceSpeed) {
+        LocalTime currentTime = LocalTime.now();
+        table.put(key, 0, currentTime.toSecondOfDay());
+        table.put(key, 1, joystickPosition);
+        table.put(key, 2, referenceSpeed);
+        key++;
+    }
+}
